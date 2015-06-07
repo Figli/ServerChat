ac29e93f7dfefc2a2c41596fde00b792515ad513
Viktor <viktor_mag@mail.ru>
2015-06-07 20:19:01 +0300

    Initial commit

---
+++ src/UserFile.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+
+
+
+public class UserFile implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public byte[] uFile = null;
+    String uFileName = null;
+
+    public UserFile() {
+
+    }
+
+    public UserFile(String name, byte[] file) {
+        this.uFileName = name;
+        this.uFile = file;
+    }
+}
